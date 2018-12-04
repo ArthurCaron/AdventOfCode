9497773304b07322b6e4a9a3814c779ae0174aaa
Arthur Caron <a.caron@oodrive.com>
2018-12-04 16:03:40 +0100

    Add empty classes

---
+++ src/main/java/io/karon/adventofcode/eighteen/Day23.java
@@ -0,0 +1,25 @@
+package io.karon.adventofcode.eighteen;
+
+class Day23 {
+
+	static class Star1 {
+
+		static int getResult() {
+			return 0;
+		}
+
+	}
+
+	static class Star2 {
+
+		static int getResult() {
+			return 0;
+		}
+
+	}
+
+	private static String getInput() {
+		return "";
+	}
+
+}
---
+++ src/main/java/io/karon/adventofcode/fifteen/Day3.java
@@ -0,0 +1,25 @@
+package io.karon.adventofcode.fifteen;
+
+class Day3 {
+
+	static class Star1 {
+
+		static int getResult() {
+			return 0;
+		}
+
+	}
+
+	static class Star2 {
+
+		static int getResult() {
+			return 0;
+		}
+
+	}
+
+	private static String getInput() {
+		return "";
+	}
+
+}
